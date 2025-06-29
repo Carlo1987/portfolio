41bac735c666a5da8cf88df94e8f4325cbcf3cb5
Carlo1987 <carlo_loi87@yahoo.it>
2025-06-29 20:21:21 +0200

    wip_updating_curriculum

---
+++ frontend/src/app/services/global.ts
@@ -0,0 +1,20 @@
+
+const url = "http://localhost:8080"; 
+
+
+export const global = {
+
+  url_email : `${url}/email/email.php` ,
+
+  url_curriculum : `${url}/curriculum/curriculum.php`,
+  
+  project_shop : `${url}/Project_Shop/home`,
+
+  project_social : `${url}/Project_Social`,
+
+  project_holiday : "https://carloloidevholiday.com",
+
+
+  timing_animation : 4500
+
+}
